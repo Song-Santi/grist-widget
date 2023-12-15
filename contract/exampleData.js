1f3226e4be91c736170129e2a0fbba71efd59016
Song-Santi <song.pv.1067@aptechlearning.edu.vn>
2023-12-15 22:19:42 +0700

    test match data

--- contract/exampleData.js
+++ contract/exampleData.js
@@ -1,4 +1,17 @@
 const exampleData = {
+    Contract: 146,
+    DateInText:'Hôm nay, ngày 01 tháng 09 năm 2023, chúng tôi gồm: ',
+    SideA: 'Ông Vương Bá Quang, sinh năm 1951, mang CCCD số 001051000519 do Cục cảnh sát QLHC về TTXH cấp ngày 24/06/2021, có hộ khẩu thường trú tại: 382 đường 21A cụm 4 thôn 3, Thạch Hòa, Thạch Thất, Hà Nội. Tel/Zalo: 098 963 9985 (Ms Quỳnh 090 434 6979).',
+    SideB: 'Ông/bà: LÊ PHƯƠNG MAI…………………., ngày sinh: 18/09/2005…..………..,\nCCCD số: 001305013700…….Nơi cấp: QLHC về TTXH…….. Cấp ngày 29/04/2021……...,\nHộ khẩu thường trú tại: Báo Đáp Kiêu Kỵ, Gia Lâm, Hà Nội……….………….….\nTel: 0344981251 (Mẹ Nguyễn Thu Hằng 0333979004).\nTrường: FPT.....Mail: phuongmai180905@gmail.com Facebook:……………………………',
+    Room: 'P02',
+    RoomHandover: 'Diện tích: 20m2 Tại KTX Nhà Ông Bà địa chỉ 382 đường 21A cụm 4 thôn 3, Thạch Hòa, Thạch Thất, Hà Nội. Trang thiết bị kèm theo: 01 bộ chìa khóa phòng, 02 thẻ từ thang máy, 02 bộ giường tủ bàn học, 01 điều hòa kèm điều khiển, 01 bình nóng lạnh, 01 hệ thống điện nước, các bóng đèn, hệ thống công tắc, bồn rửa mặt, bồn vệ sinh.',
+    RoomPriceText:'Giá phòng trọ: 2.500.000 đồng/phòng/tháng(Bằng chữ: Hai triệu, năm trăm ngàn đồng chẵn)',
+    ItemFee: 'Tiền điện: 2.500 đồng/kwh theo chỉ số công tơ.\nTiền nước: 50.000 đồng/người/tháng.\nTiền Internet: 50.000 đồng/người/tháng.\nTiền vệ sinh: 10.000 đồng/người/tháng.',
+    AccountBank:'Trần Thị Lạm – 104870736609 - Vietinbank - CN Bắc Hà Nội',
+    DateInFormat:'1/9/2023',
+
+    ///////////////////////////////
+    /* 
     Number: 14999,
     Issued: Date.parse('2020-10-12') / 1000,
     Due: Date.parse('2020-11-12') / 1000,
@@ -42,5 +55,6 @@ const exampleData = {
     Deduction: null,
     Taxes: null,
     Total: 615,
+    */
   };
   
