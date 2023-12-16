28c00a97fb1561745994fb3367dd6d057da1f0f0
Song-Santi <song.pv.1067@aptechlearning.edu.vn>
2023-12-16 12:57:53 +0700

    start build contract

--- contract/exampleData.js
+++ contract/exampleData.js
@@ -9,52 +9,5 @@ const exampleData = {
     ItemFee: 'Tiền điện: 2.500 đồng/kwh theo chỉ số công tơ.\nTiền nước: 50.000 đồng/người/tháng.\nTiền Internet: 50.000 đồng/người/tháng.\nTiền vệ sinh: 10.000 đồng/người/tháng.',
     AccountBank:'Trần Thị Lạm – 104870736609 - Vietinbank - CN Bắc Hà Nội',
     DateInFormat:'1/9/2023',
-
-    ///////////////////////////////
-    /* 
-    Number: 14999,
-    Issued: Date.parse('2020-10-12') / 1000,
-    Due: Date.parse('2020-11-12') / 1000,
-  
-    Invoicer: {
-      Name: 'Thunderous Applause',
-      Street1: '812 Automated Rd',
-      Street2: null,
-      City: 'New York',
-      State: 'NY',
-      Zip: '10003',
-      Email: 'applause@thunder.com',
-      Phone: '+1-800-111-1111',
-      Website: 'applause.com',
-    },
-  
-    Client: {
-      Name: 'Monkeys Juggling',
-      Street1: '100 Banana St',
-      City: 'Bananaberg',
-      State: 'NJ',
-      Zip: '07048',
-    },
-  
-    Items: [
-      {
-        Description: 'Wolf Whistle',
-        Price: 35,
-        Quantity: 3,
-        Total: 105,
-      },
-      {
-        Description: 'Bravo',
-        Price: 30,
-        Quantity: 17,
-        Total: 510,
-      },
-    ],
-  
-    Subtotal: 615,
-    Deduction: null,
-    Taxes: null,
-    Total: 615,
-    */
   };
   
